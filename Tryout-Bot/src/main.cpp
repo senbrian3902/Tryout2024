609e98eb399a14f62c9e3b61826b9499ffd46c49
Nguyenpk <sensiblebran@gmail.com>
2024-08-21 21:58:59 +0700

    nocom

--- Tryout-Bot/src/main.cpp
+++ Tryout-Bot/src/main.cpp
@@ -10,6 +10,8 @@ BluetoothSerial SerialBT;
 
 char cmd;
 
+int speed = 100;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -23,83 +25,80 @@ void loop() {
     cmd = char(SerialBT.read());
     // Serial.println(cmd);
 
-      switch (cmd) {
+    switch (cmd) {
       case 'F':
-        dc1.Run(100);
-        dc2.Run(100);
+        dc1.Run(speed);
+        dc2.Run(speed);
         break;
       case 'B':
-        dc1.Run(-100);
-        dc2.Run(-100);
+        dc1.Run(-speed);
+        dc2.Run(-speed);
         break;
       case 'R':
-        dc1.Run(-100);
-        dc2.Run(100);
+        dc1.Run(-speed);
+        dc2.Run(speed);
         break;
       case 'L':
-        dc1.Run(100);
-        dc2.Run(-100);
+        dc1.Run(speed);
+        dc2.Run(-speed);
         break;
       case 'S':
         dc1.Stop();
         dc2.Stop();
         break;
       case 'G':
-        dc2.Run(150);
+        dc2.Run(speed + 50);
         break;
       case 'I':
-        dc1.Run(150);
+        dc1.Run(speed + 50);
         break;
       case 'H':
-        dc1.Run(-150);
+        dc1.Run(-speed - 50);
         break;
       case 'J':
-        dc2.Run(-150);
+        dc2.Run(-speed - 50);
         break;
       case 'D':
         dc1.Stop();
         dc2.Stop();
-      }
-        
-      // case '0':
-      //   100 = 0;
-      // case '1':
-      //   100 = 100;
-      // case '2':
-      //   100 = 200;
-      // case '3':
-      //   100 = 300;
-      // case '4':
-      //   100 = 400;
-      // case '5':
-      //   100 = 500;
-      // case '6':
-      //   100 = 600;
-      // case '7':
-      //   100 = 700;
-      // case '8':
-      //   100 = 800;
-      // case '9':
-      //   100 = 900;
-      // case 'q':
-      //   100 = 1000;
+      case '0':
+        speed = 0;
+      case '1':
+        speed = 100;
+      case '2':
+        speed = 200;
+      case '3':
+        speed = 300;
+      case '4':
+        speed = 400;
+      case '5':
+        speed = 500;
+      case '6':
+        speed = 600;
+      case '7':
+        speed = 700;
+      case '8':
+        speed = 800;
+      case '9':
+        speed = 900;
+      case 'q':
+        speed = 1000;
       
       // // case 'X':
       // //   for(long long angle = 0; angle <= 90; angle++) {
       // //     myServo.write(angle);
       // //     delay(10);
       // //   }
-      // //   delay(1000);
+      // //   delay(speed0);
       // //   for(long long angle = 90; angle > 0; angle--) {
       // //     myServo.write(angle);
       // //     delay(10);
       // //   }
       // //   
-      // default:
-      //   dc1.Stop();
-      //   dc2.Stop();
-        
-    // }
+      default:
+        dc1.Stop();
+        dc2.Stop(); 
+    }
   }
 }
 
